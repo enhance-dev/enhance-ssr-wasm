d372d2ccd77b3ed91b558304aab4aec537eb40a4
Ryan Bethel <ryan.bethel@gmail.com>
2024-02-21 12:05:11 -0500

    tests

--- test/basic.test.mjs
+++ test/basic.test.mjs
@@ -1,21 +1,25 @@
-import { exec } from 'child_process'
 import assert from 'node:assert'
 import { test } from 'node:test'
+import createPlugin from "@extism/extism";
 
-const expected = `<html><head></head><body><my-header enhanced="✨"><h1>Hello World</h1> </my-header></body></html>`
-test('Read Input Text', async () => {
-  const { stdout, stderr } = await runShellCommand('extism call dist/enhance-ssr.wasm ssr --input="$(cat test/test.json)" --wasi')
-  assert.equal(stdout.trim(), expected)
+const plugin = await createPlugin("dist/enhance-ssr.wasm", {
+  useWasi: true,
 })
 
-function runShellCommand(command) {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve({ stdout, stderr });
-    });
-  });
+const input = {
+  markup: "<my-header>Hello World</my-header>",
+  elements: {
+    "my-header":
+      "function MyHeader({ html }) { return html`<h1><slot></slot></h1> ` }",
+  },
+  initialState: {},
 }
+const expected = `<html><head></head><body><my-header enhanced="✨"><h1>Hello World</h1> </my-header></body></html>`
+
+test('Read Input Text', async () => {
+  const output = await plugin.call("ssr", JSON.stringify(input))
+  assert.equal(output.text(), expected)
+})
+
+
+
